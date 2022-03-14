2fe0ba5e4423d9e01c27991ef6d033edf484d101
DESKTOP-DU4AR9B\User <leehan0617@naver.com>
2022-03-14 14:19:58 +0900

    add sub menu

--- src/component/home/HomeTitle.js
+++ src/component/home/HomeTitle.js
@@ -1,15 +1,16 @@
+import { useState } from "react";
 import styled from "styled-components";
-import { Grid } from "@mui/material";
+import { Menu, MenuItem, Grid } from "@mui/material";
 import partnerLogo from "../../image/partner_logo.svg";
 import detailLogo from "../../image/partner_detail.svg";
 
 const PartnerLogo = styled.img`
   float: left;
-`
+`;
 const ShopInfo = styled.div`
   position: relative;
   left: 12px;
-`
+`;
 const ShopTitleText = styled.div`
   position: relative;
   top: 2px;
@@ -26,7 +27,7 @@ const ShopTitleText = styled.div`
 const ShopAddressText = styled.div`
   position: relative;
   top: 6px;
-  color: #B4B4B4;
+  color: #b4b4b4;
   font-size: 12px;
   line-height: 16px;
   letter-spacing: -0.02em;
@@ -37,16 +38,44 @@ const DetailLogo = styled.img`
 `;
 
 function HomeTitle() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const openMoreMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const closeMoreMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const logout = () => {
+    console.log("TODO logout");
+    closeMoreMenu();
+  };
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={10}>
         <PartnerLogo alt="partner_logo" src={partnerLogo} />
         <ShopInfo>
           <ShopTitleText>레드텅 부티크 센텀점</ShopTitleText>
-          <ShopAddressText>서울특별시 성동구 옥수동 291-9</ShopAddressText>  
+          <ShopAddressText>서울특별시 성동구 옥수동 291-9</ShopAddressText>
         </ShopInfo>
       </Grid>
-      <Grid item xs={2} sx={{ marginTop: "16px", textAlign: "right" }}><DetailLogo src={detailLogo} /></Grid>
+      <Grid item xs={2} sx={{ marginTop: "16px", textAlign: "right" }}>
+        <DetailLogo id="detail-logo" src={detailLogo} onClick={openMoreMenu} />
+        <Menu
+          id="more_menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={closeMoreMenu}
+          MenuListProps={{
+            "aria-labelledby": "detail-logo",
+          }}>
+          <MenuItem onClick={closeMoreMenu}>Profile</MenuItem>
+          <MenuItem onClick={logout}>Logout</MenuItem>
+        </Menu>
+      </Grid>
     </Grid>
   );
 }
